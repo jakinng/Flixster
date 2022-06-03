ba51e9f33fb1fc743896540fd3cd2c9c76622bb1
xueenwu <jakin.ng03@gmail.com>
2022-06-03 16:09:07 -0700

    Added an image to the detailed view

--- app/src/main/java/com/example/flixster/MovieDetailsActivity.java
+++ app/src/main/java/com/example/flixster/MovieDetailsActivity.java
@@ -2,11 +2,16 @@ package com.example.flixster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CenterInside;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.example.flixster.models.Movie;
 
 import org.parceler.Parcels;
@@ -19,6 +24,7 @@ public class MovieDetailsActivity extends AppCompatActivity {
     TextView tvTitle;
     TextView tvOverview;
     RatingBar rbVoteAverage;
+    ImageView ivPoster;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +35,7 @@ public class MovieDetailsActivity extends AppCompatActivity {
         tvTitle = (TextView) findViewById(R.id.tvTitle);
         tvOverview = (TextView) findViewById(R.id.tvOverview);
         rbVoteAverage = (RatingBar) findViewById(R.id.rbVoteAverage);
+        ivPoster = (ImageView) findViewById(R.id.ivPoster);
 
         // unwrap the movie passed in via intent, using its simple name as a key
         movie = (Movie) Parcels.unwrap(getIntent().getParcelableExtra(Movie.class.getSimpleName()));
@@ -41,5 +48,25 @@ public class MovieDetailsActivity extends AppCompatActivity {
         // convert from 0 - 10 to 0 - 5 range
         float voteAverage = movie.getVoteAverage().floatValue();
         rbVoteAverage.setRating(voteAverage / 2.0f);
+
+        // show poster image
+//        int corner_radius = 30;
+//        String imageURL;
+//        int placeholder;
+//        int orientation = context.getResources().getConfiguration().orientation;
+//        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+//            imageURL = movie.getPosterPath();
+////                imageURL = "as;djfa;slkjdflasd";
+//            placeholder = R.drawable.flicks_movie_placeholder;
+//        } else {
+//            imageURL = movie.getBackdropPath();
+//            placeholder = R.drawable.flicks_backdrop_placeholder;
+//        }
+
+        Glide.with(this)
+                .load(movie.getPosterPath())
+//                .placeholder(placeholder)
+//                .transform(new CenterInside(), new RoundedCorners(corner_radius))
+                .into(ivPoster);
     }
 }
